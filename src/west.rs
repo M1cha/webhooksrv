7bae72a76c92c2b3c7525feb862496a3af306f5e
Michael Zimmermann <michael.zimmermann@grandcentrix.net>
2021-09-30 18:33:25 +0200

    west: add support for `submodules`

--- src/west.rs
+++ src/west.rs
@@ -18,6 +18,8 @@ pub struct Project {
     pub west_commands: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub import: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub submodules: Option<bool>,
 }
 
 #[derive(Clone, Default, Debug, serde::Deserialize, serde::Serialize)]
